e30b5fc704fc8704c059ff6349bcfd0c272fbdbb
Harsh <harsh.hbs@gmail.com>
2017-10-26 16:59:29 -0400

    Initial Commit

---
+++ Counter++/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Counter++
+//
+//  Created by Harsh Shah on 2017-10-26.
+//  Copyright © 2017 Harsh Shah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
